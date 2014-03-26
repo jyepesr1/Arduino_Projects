d331a36d01f028d0899694bcf05e6a90a448c9bf
johan yepes <johanyepes@hotmail.com>
2014-03-25 21:38:37 -0500

    Encender Led
    encender un led mediante la comunicación serial

---
+++ led_serial.ino
@@ -0,0 +1,27 @@
+
+int input;
+ 
+void setup(){
+ 
+pinMode(13, OUTPUT); // Declaramos que utilizaremos el pin 13 como salida
+ 
+Serial.begin(9600);
+}
+ 
+void loop(){
+  
+  if (Serial.available()>0){
+ 
+    input=Serial.read();
+ 
+    if (input=='e'){
+ 
+      digitalWrite(13, HIGH); //Si el valor de input es e, se enciende el led
+ 
+    } else if(input=='a'){
+ 
+      digitalWrite(13, LOW); //Si el valor de input es diferente de 1, se apaga el LED
+ 
+    }
+  }
+}
